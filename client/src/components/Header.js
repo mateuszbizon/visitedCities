305da2080d8eedb60c796222d55831b8ec1339fe
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-10-30 12:24:33 +0100

    Added buttons for cities and search cities

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 function Header({ listActive, setListActive }) {
+    const navigate = useNavigate();
 
     function handleListActive() {
         setListActive(!listActive);
@@ -10,11 +12,11 @@ function Header({ listActive, setListActive }) {
     <div className='map-header'>
         <div className='map-header__logo-side'>
             <i className="fa-solid fa-bars map-header__icon map-header__burger-icon" onClick={handleListActive}></i>
-            <h2>visitedCities</h2>
+            <h2 className='map-header__logo'>visitedCities</h2>
         </div>
         <div className="map-header__right-side">
-            <a href='#' className='map-header__link'>Log Out</a>
-            <i className="fa-solid fa-right-from-bracket map-header__icon map-header__logout-icon" aria-label='wyloguj się'></i>
+            <a href='/' className='map-header__link'>Wyloguj się</a>
+            <i className="fa-solid fa-right-from-bracket map-header__icon map-header__logout-icon" aria-label='wyloguj się' onClick={() => navigate("/")}></i>
         </div>
     </div>
   )
--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function List({ listActive }) {
+  const [isSearchActive, setIsSearchActive] = useState(false);
+
+  function changeSearchActive() {
+    setIsSearchActive(!isSearchActive);
+  }
+
   return (
     <div className={!listActive ? "list" : "list list-show"}>
-        List
+        <div className='list__main-btns'>
+          <button className={isSearchActive ? "list__main-btn" : "list__main-btn list__main-btn--active"} onClick={changeSearchActive}>Miasta</button>
+          <button className={!isSearchActive ? "list__main-btn" : "list__main-btn list__main-btn--active"} onClick={changeSearchActive}>Szukaj</button>
+        </div>
     </div>
   )
 }
