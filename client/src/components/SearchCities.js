463736fe8720a67b3a25ee3df5f9084ed94791e7
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-10-31 14:41:36 +0100

    Added selecting city to add one

--- client/src/components/SearchCities.js
+++ client/src/components/SearchCities.js
@@ -6,6 +6,7 @@ function SearchCities() {
     const [searchedCities, setSearchedCities] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [selectedCity, setSelectedCity] = useState(null);
+    const [selectedCityIndex, setSelectedCityIndex] = useState(null);
 
     function handleGetLocationsBySearch() {
         if (searchValue === "") return;
@@ -18,13 +19,30 @@ function SearchCities() {
             })
     }
 
+    function toggleSelectedCity(city, index) {
+        if (selectedCityIndex === index) {
+            setSelectedCity(null);
+            setSelectedCityIndex(null);
+
+            return;
+        }
+
+        setSelectedCity(city);
+        setSelectedCityIndex(index);
+    }
+
   return (
     <div className='search-cities'>
         <input className='search-cities__input' type='text' onChange={(e) => setSearchValue(e.target.value)} value={searchValue} placeholder='Szukaj miasta' />
-        <button className='search-cities__search-btn' onClick={handleGetLocationsBySearch} disabled={isLoading}>Szukaj</button>
+        <button className='search-cities__search-btn' onClick={handleGetLocationsBySearch} disabled={isLoading || searchValue === ""}>Szukaj</button>
         <div className='search-cities__content'>
             {searchedCities.map((city, index) => (
-                <div className='search-cities__city' key={index}>{city.name}</div>
+                <div 
+                    className={index === selectedCityIndex ? 'search-cities__city search-cities__city--selected' : "search-cities__city"} 
+                    key={index} 
+                    onClick={() => toggleSelectedCity(city, index)}>
+                {city.name}
+                </div>
             ))}
         </div>
         <button className='search-cities__new-city-btn' disabled={!selectedCity}>Dodaj miasto</button>
