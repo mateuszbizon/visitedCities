b3759a3f2f0895a476091f9c7c4b4fecad71e367
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-11-08 12:12:19 +0100

    Added aria label to burger icon

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,14 +12,14 @@ function Header({ listActive, setListActive }) {
     <div className='map-header'>
         <div className='map-header__logo-side'>
             <button className='map-header__btn' onClick={handleListActive}>
-                <i className="fa-solid fa-bars"></i>
+                <i className="fa-solid fa-bars" aria-label='burger ikonka do otwierania bocznego menu'></i>
             </button>
             <h2 className='map-header__logo'>visitedCities</h2>
         </div>
         <div className="map-header__right-side">
-            <a href='/' className='map-header__link'>Wyloguj się</a>
+            <button className='map-header__btn-link' onClick={() => navigate("/")}>Wyloguj się</button>
             <button className='map-header__btn map-header__logout-btn' onClick={() => navigate("/")}>
-                <i className="fa-solid fa-right-from-bracket" aria-label='wyloguj się'></i>
+                <i className="fa-solid fa-right-from-bracket" aria-label='ikonka do wylogowania się'></i>
             </button>
         </div>
     </div>
