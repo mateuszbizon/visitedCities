23a4730413eafd8b714493dd509e3e29422dc5d7
ursgre <86769021+ursgre@users.noreply.github.com>
2023-11-19 21:47:07 +0100

    Token
    download the token that is returned after logging in, sending this token to the server and then, if everything is correct, saving the same token in local storage

--- client/src/routes/Login.js
+++ client/src/routes/Login.js
@@ -1,22 +1,48 @@
-import React, { useState } from 'react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { GoogleLogin } from '@react-oauth/google';
+import React from 'react';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
 
+function Login() {
+  const handleLoginSuccess = async (credentialResponse) => {
+    try {
+    
+      console.log('Token received:', credentialResponse);
 
-function Login(){
-  return(
+      const token = credentialResponse?.credentials?.accessToken; 
+
+      
+      const response = await fetch('YOUR_SERVER_ENDPOINT', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token }), 
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+       
+        localStorage.setItem('accessToken', token);
+        console.log('Token saved to local storage');
+      } else {
+        console.log('Token validation failed on the server');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
     <GoogleOAuthProvider clientId="255299074998-i2png2ca07osvp5b3rfa4fq8qi4btte6.apps.googleusercontent.com">
-    <GoogleLogin
-      onSuccess={credentialResponse => {
-      console.log(credentialResponse);
-  }}
-      onError={() => {
-      console.log('Login Failed');
-  }}
-    />
+      <GoogleLogin
+        onSuccess={handleLoginSuccess}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
     </GoogleOAuthProvider>
-    );
+  );
 }
 
 export default Login;
