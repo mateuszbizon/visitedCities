9f2e01a03fb5d4a21cf06b5c34593f5fec66c3bb
ursgre <86769021+ursgre@users.noreply.github.com>
2023-11-19 20:16:32 +0100

    Fix function login-oauth

--- client/src/Login-oauth.js
+++ client/src/Login-oauth.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Login from "./routes/Login";
-
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  function Login-oauth() {
+    return(
     <GoogleOAuthProvider clientId="255299074998-i2png2ca07osvp5b3rfa4fq8qi4btte6.apps.googleusercontent.com">
     <Login />
     </GoogleOAuthProvider>
-  </React.StrictMode>
-);
+    );
+  }
+
+  export default Login-oauth
+  
